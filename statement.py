93311410c1b7a622a2674269c42b2fa3f39fb1a3
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-09-29 16:22:48 +0200

    Migrate to new on_change API

--- statement.py
+++ statement.py
@@ -15,7 +15,7 @@ class StatementMoveLine:
 
     @fields.depends('invoice', 'payment')
     def on_change_invoice(self):
-        changes = super(StatementMoveLine, self).on_change_invoice()
+        super(StatementMoveLine, self).on_change_invoice()
         if self.invoice and self.payment and self.payment.processing_move:
             # compatibility with account_bank_statement_payment
             clearing_perncent = (
@@ -24,26 +24,20 @@ class StatementMoveLine:
             if clearing_perncent == Decimal(1):
                 for line in self.payment.processing_move.lines:
                     if line.account != self.payment.line.account:
-                        changes['account'] = line.account.id
-                        changes['account.rec_name'] = line.account.rec_name
                         self.account = line.account
                         break
-        return changes
 
     @fields.depends('payment', 'party', 'account', 'amount',
         '_parent_line._parent_statement.journal',
         methods=['invoice'])
     def on_change_payment(self):
-        changes = super(StatementMoveLine, self).on_change_payment()
+        super(StatementMoveLine, self).on_change_payment()
         if (self.payment and not self.invoice and self.payment.processing_move
                 and not self.account):
             for line in self.payment.processing_move.lines:
                 if line.account != self.payment.line.account:
-                    changes['account'] = line.account.id
-                    changes['account.rec_name'] = line.account.rec_name
                     self.account = line.account
                     break
-        return changes
 
     def create_move(self):
         pool = Pool()
